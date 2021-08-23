9023ab1ee43b9045a22b6bcb7cbc3ffd89a7ae93
Roushan Raj <roushanrj776@gmail.com>
2021-08-23 11:41:57 +0530

    New Test case added

---
+++ src/test/java/com/example/demo/UnitTesing.java
@@ -0,0 +1,63 @@
+package com.example.demo;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.example.demo.controller.StudentController;
+import com.example.demo.model.Student;
+import com.example.demo.service.StudentOperation;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes= PostgresIntegrationApplication.class)
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application.properties")
+public class UnitTesing {
+	@Autowired
+	private MockMvc mock;
+	
+	@MockBean
+	private StudentOperation operation;
+	
+	@MockBean
+	private StudentController controller;
+	
+	@Test
+	public void getStudentsTest() throws Exception
+	{
+		List<Student> studentlist= getValue();
+		when(controller.getStudents()).thenReturn(studentlist);
+		this.mock
+		.perform(MockMvcRequestBuilders.get("/students")
+				.contentType(MediaType.APPLICATION_JSON))
+		.andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(2)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].name", Matchers.equalTo("DummyName")))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[1].name", Matchers.equalTo("DummyName1")));
+	}
+	
+	private List<Student> getValue(){
+		List<Student> studentlist= new ArrayList<>();
+		Student s1= new Student("DummyName",45,4,2014);
+		Student s2= new Student("DummyName1",46,5,2013);
+		studentlist.add(s1);
+		studentlist.add(s2);
+		return studentlist;
+		
+	}
+}
